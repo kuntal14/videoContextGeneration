068c1b517c5f6f4504d1b81a2c4679e17d47b572
kuntal14 <sumankuntal6@gmail.com>
2025-12-04 11:55:33 +0530

    first commit of 4th dec

--- context.py
+++ context.py
@@ -3,7 +3,6 @@ import shutil
 import frameExtractor as fe 
 import transcription as tr
 import os
-import subprocess
 from pathlib import Path
 
 keyframes_folder_path = fe.keyframes_folder_path
@@ -27,11 +26,8 @@ def get_transcript(video_path):
 if __name__ == "__main__":
     video_path = Path.home()/ "Downloads" / "demo.mov"
     t0 = time()
-    # get_frames()
-    # get_transcript(video_path)
-    # 46.88424015045166 -> time for synchronous exec
-    subprocess.run(["venv_311/bin/python3", "transcription.py"])
-    subprocess.run(["venv_311/bin/python3", "frameExtractor.py"])
+    get_frames()
+    get_transcript(video_path)
     t1 = time()
     print(t1 - t0)
 
